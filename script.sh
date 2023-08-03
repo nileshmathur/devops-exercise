598cc2db324f6073703fce9eff92f15ac3afe321
Nilesh Mathur <64744992+nileshmathur@users.noreply.github.com>
2023-08-03 14:36:41 +0530

    Updated latest bash script

--- script.sh
+++ script.sh
@@ -1,9 +1,9 @@
-sudo yum update -y
-sudo yum install python3 -y
-sudo yum install pip -y
-sudo pip install ansible -y
-sudo yum install git -y
+sudo yum update
+sudo yum install pip docker git -y
+sudo pip install ansible
+sudo systemctl start docker
+sudo systemctl enable docker
 sudo git clone https://github.com/nileshmathur/devops-exercise.git
 cd devops-exercise/
 sudo ansible-playbook playbook.yml
-sudo docker-compose up
+sudo docker-compose up -d
